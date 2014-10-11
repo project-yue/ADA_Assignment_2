2646994e7d0c4ab0bda7e5db8b273dffa797819e
Yue <makingitbettergo@gmail.com>
2014-10-11 22:07:54 +1300

    created node class for manipulating kruskal's greedy algorithm

---
+++ ada_assignment_2/src/assignment_2/pqs/WeightNode.java
@@ -0,0 +1,25 @@
+package assignment_2.pqs;
+
+public class WeightNode implements Comparable<WeightNode> {
+
+	public int from;
+	public int to;
+	public double weight;
+
+	public WeightNode(int from, int to, double weight) {
+		this.from = from;
+		this.to = to;
+		this.weight = weight;
+	}
+
+	@Override
+	public int compareTo(WeightNode o) {
+		if (this.weight > o.weight)
+			return 1;
+		else if (this.weight < o.weight)
+			return -1;
+		else
+			return 0;
+	}
+
+}
